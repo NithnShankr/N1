985041a44b422d7761ec0c410e01450d1b8bc950
Nithin N S <developer.nithin@gmail.com>
2026-04-10 13:18:17 +0700

    8th Wall Project

---
+++ 8th Wall Project/src/Picker.ts
@@ -0,0 +1,23 @@
+// This is a component file. You can use this file to define a custom component for your project.
+// This component will appear as a custom component in the editor.
+
+import * as ecs from '@8thwall/ecs'  // This is how you access the ecs library.
+
+ecs.registerComponent({
+  name: 'Picker',
+  // schema: {
+  // },
+  // schemaDefaults: {
+  // },
+  // data: {
+  // },
+  // add: (world, component) => {
+  // },
+  // tick: (world, component) => {
+  // },
+  // remove: (world, component) => {
+  // },
+  // stateMachine: ({world, eid, schemaAttribute, dataAttribute}) => {
+  //   ecs.defineState('default').initial()
+  // },
+})
